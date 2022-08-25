04efb04d9d80d3b922a1dd064128f12ec01cc7d8
pallavijawanjal <pallavij@gmail.com>
2022-08-25 10:43:41 +0530

    Add project files.

---
+++ Day19UserRegistration/UC5_PasswordRule1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19UserRegistration
+{
+    class UC5_PasswordRule1
+    {
+        // A password should have minimum 8 characters 
+        public static string Regex_Password = "^[a-zA-z0-9]{8,16}$";
+
+        public bool validatePassword(string password)
+        {
+            return Regex.IsMatch(password, Regex_Password);
+        }
+    }
+}
