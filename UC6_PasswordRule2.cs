dfb98aab6f2765c4ca4d573caac411c2272ad852
pallavijawanjal25 <109516737+pallavijawanjal25@users.noreply.github.com>
2022-08-25 11:53:04 +0530

    Adding UC6

---
+++ UC6_PasswordRule2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19UserRegistration
+{
+    class UC6_PasswordRule2
+    {
+        // A password should have at least one uppercase character
+        public static string Regex_Password = "(?=.*[A-Z])(?=.[0-9a-z]{7,})";
+
+        public bool validatePassword(string password)
+        {
+            return Regex.IsMatch(password, Regex_Password);
+        }
+    }
+}
